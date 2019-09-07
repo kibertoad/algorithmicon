3431d6a8488afd9d9f5a448aaf5a493fb8dc8377
Igor Savin <iselwin@gmail.com>
2019-09-08 01:03:55 +0200

    Add comment for arraysEqual monoku

--- code-monoku/arrays/equalCheck.js
+++ code-monoku/arrays/equalCheck.js
@@ -1,3 +1,6 @@
+/**
+ * Returns true if arrays have equal elements in them, in same order, or false otherwise
+ */
 function arraysEqual(array1, array2) {
   return array1.length === array2.length && array1.every((value, index) => value === array2[index])
 }
