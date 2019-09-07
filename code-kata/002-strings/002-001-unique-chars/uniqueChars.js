a1416850422b2ce5e872ba9e8fd376fb949e0bad
Igor Savin <iselwin@gmail.com>
2019-09-07 22:43:39 +0200

    Add one more kata

---
+++ code-kata/002-strings/002-001-unique-chars/uniqueChars.js
@@ -0,0 +1,18 @@
+/*
+
+Difficulty: Easy (2)
+Problem statement: Determine if given string contains only unique characters (case-sensitive)
+Output format: true or false
+
+*/
+
+function implementationFn(inputLines) {
+  const inputString = inputLines[0]
+
+  // Your implementation
+  // return result
+}
+
+module.exports = {
+  implementationFn
+}
